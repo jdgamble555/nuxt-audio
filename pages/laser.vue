e7aebf88b791025d4bd93848166e01ef70f04db0
Jonathan Gamble <jdgamble555@gmail.com>
2025-03-28 20:18:29 -0500

    laser gun

--- pages/laser.vue
+++ pages/laser.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <h2>Laser</h2>
-        <audio autoplay ref="audio" src="/laser-gun.wav" preload="auto" />
+        <h2>Laser Gun</h2>
+        <audio loop autoplay ref="audio" src="/laser-gun.wav" preload="auto" />
+        <img src="/laser-gun.gif" alt="laser gun" />
     </div>
 </template>
