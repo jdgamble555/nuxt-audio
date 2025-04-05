87fc91483dea2f078bb751195e78c5aa588d96bd
Jonathan Gamble <jdgamble555@gmail.com>
2025-04-05 17:25:04 -0500

    mario

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h2>Mario</h2>
+        <h2 class="text-2xl my-3">Let's go Mario!</h2>
         <audio autoplay ref="audio" src="/mario.wav" preload="auto" controls />
     </div>
 </template>
